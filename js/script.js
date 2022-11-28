86089ed40474e86b04de92943c33029d2305c990
sik <daesik99@naver.com>
2022-11-28 17:28:34 +0900

    update/ jquery plugin use

--- js/script.js
+++ js/script.js
@@ -1,9 +1,14 @@
-// $(()=>{});
 $(document).ready(function () {
+  /* slide */
   $("#slide > ul").slidy({
     children: "li",
     width: 2000,
     height: 880,
     animation: "fade",
+  });
+  /* header fix */
+  $("#headerArea").scrollToGiveClass({
+    baseline: 90,
+    class: "active",
   });
 });
