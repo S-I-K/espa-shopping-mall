2ac9ce659f6038a6f1e4d8b7d10aad531837ffb1
sik <daesik99@naver.com>
2022-11-29 18:04:46 +0900

    update/ jquery library use

--- js/script.js
+++ js/script.js
@@ -1,14 +1,48 @@
 $(document).ready(function () {
   /* slide */
-  $("#slide > ul").slidy({
-    children: "li",
-    width: 2000,
-    height: 880,
-    animation: "fade",
+  $("#slide > ul").bxSlider({
+    auto: "true",
+    nextText: "",
+    prevText: "",
   });
   /* header fix */
   $("#headerArea").scrollToGiveClass({
     baseline: 90,
     class: "active",
+  });
+  /* scroll effect */
+  $("#stylePick .title").scrollToGiveClass({
+    baseline: "middle",
+    add: 300,
+  });
+
+  $(".style-contents").scrollToGiveClass({
+    baseline: "middle",
+    add: 300,
+  });
+  $("#whats-content .title").scrollToGiveClass({
+    baseline: "middle",
+    add: 300,
+  });
+  $("#whats-inner").scrollToGiveClass({
+    baseline: "middle",
+    add: 300,
+  });
+  $("#best-area").scrollToGiveClass({
+    baseline: "middle",
+    add: 300,
+  });
+  $("#best-area").scrollToGiveClass({
+    baseline: "middle",
+    add: 300,
+  });
+  $(".style-contents > ul").bxSlider({
+    auto: "true",
+    maxSlides: 3,
+    slideWidth: 500,
+    moveSlides: 1,
+    speed: 700,
+    pause: 5000,
+    controls: false,
   });
 });
